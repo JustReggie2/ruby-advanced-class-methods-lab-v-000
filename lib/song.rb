89cbf7dd0b01624fb0eb42fdfd943acf0936b0d7
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-07-21 23:31:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
     #else
       #self.create_by_name(name)
     #end
-    self.create_by_name(name) || self.find_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
